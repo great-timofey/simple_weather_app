7fd1a18fd2aa225c72641936c4ae642786050259
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-09-22 02:01:10 +0700

    improved saving/deleting logic

--- lib/blocs/weather_bloc.dart
+++ lib/blocs/weather_bloc.dart
@@ -4,48 +4,57 @@ import 'dart:convert' show json;
 import 'package:rxdart/rxdart.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:simple_weather_app/utils/utils.dart';
 import 'package:simple_weather_app/models/models.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class WeatherBloc {
   List<City> _cities = [];
-  Map<String, String> _citiesData;
+  Map<String, String> _persistedCities;
   Map<String, double> _currentLocation;
-  final _citiesSubject = BehaviorSubject<List<dynamic>>();
+  final _citiesSubject = BehaviorSubject<List<City>>();
   final _isLoadingSubject = BehaviorSubject<bool>(seedValue: false);
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   Stream<bool> get isLoading => _isLoadingSubject.stream;
-  Stream<List<dynamic>> get cities => _citiesSubject.stream;
+  Stream<List<City>> get cities => _citiesSubject.stream;
 
   WeatherBloc() {
     // print('bloc started');
     _isLoadingSubject.add(true);
-    _getCities();
+    _getCitiesFromPrefs().then((_) => _getCitiesFromApi());
     _getLocation();
   }
 
-  _getCities() async {
-    await _prefs.then((SharedPreferences prefs) {
-      final Set<String> keys = prefs.getKeys();
-      Map<String, String> citiesData = Map.fromIterable(
-        keys,
-        key: (city) => city,
-        value: (city) => prefs.getString(city),
-      );
-      _citiesData = citiesData;
-    });
-
+  _getCitiesFromApi() async {
     final List<Future> futureCities = [];
-    _citiesData
+    _persistedCities
         .forEach((name, coords) => futureCities.add(_getCity(name, coords)));
     final List<City> resultCities = List.from(await Future.wait(futureCities));
 
     _cities = resultCities;
-    _citiesSubject.add((_cities));
+    _citiesSubject.add(_cities);
     _isLoadingSubject.add(false);
+  }
+
+  _getCitiesFromPrefs() async {
+    SharedPreferences prefs = await _prefs;
+    final Set<String> keys = prefs.getKeys();
+    _convertPrefsToPersistedCities(
+      keys,
+      prefs,
+    );
+  }
+
+  void _convertPrefsToPersistedCities(
+      Set<String> keys, SharedPreferences prefs) {
+    Map<String, String> persistedCities = Map.fromIterable(
+      keys,
+      key: (city) => city,
+      value: (city) => prefs.getString(city),
+    );
+    _persistedCities = persistedCities;
   }
 
   _getCity(String name, String coordinates) async {
@@ -70,24 +79,41 @@ class WeatherBloc {
     } catch (e) {
       _currentLocation = null;
     }
-    // print('current location is $_currentLocation');
+    print('current location is $_currentLocation');
   }
 
   void addCity() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('Moscow', '55.7558,37.6173');
-    await prefs.setString('Dubai', '25.2048,55.2708');
-    await prefs.setString('Prague', '50.0755,14.4378');
-    await prefs.setString('Murmansk', '68.9585,33.0827');
-    if (_currentLocation != null) {
-      await prefs.setString('somewhere', _formatLocation());
+    if (_cities.length != _persistedCities.length) {
+      _isLoadingSubject.add(true);
+      SharedPreferences refreshedPrefs = await SharedPreferences.getInstance();
+      final Set<String> refreshedKeys = refreshedPrefs.getKeys();
+      final List<Future> citiesToUpdate = [];
+      refreshedKeys.map((key) {
+        if (_persistedCities.containsKey(key) == false)
+          citiesToUpdate.add(
+              refreshedPrefs.setString(key, refreshedPrefs.getString(key)));
+      });
+      await Future.wait(citiesToUpdate);
+      _convertPrefsToPersistedCities(
+        refreshedKeys,
+        refreshedPrefs,
+      );
+      _getCitiesFromApi();
+      // await prefs.setString('Moscow', '55.7558,37.6173');
+      // await prefs.setString('Dubai', '25.2048,55.2708');
+      // await prefs.setString('Prague', '50.0755,14.4378');
+      // await prefs.setString('Murmansk', '68.9585,33.0827');
+      if (_currentLocation != null) {
+        // await prefs.setString('somewhere', _formatLocation());
+      }
     }
   }
 
   void removeCity(String cityName) async {
-    _isLoadingSubject.add(true);
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    SharedPreferences prefs = await _prefs;
     await prefs.remove(cityName);
-    _getCities();
+    _cities.remove(_cities.firstWhere((city) => city.name == cityName));
+    _citiesSubject.add(_cities);
+    // _citiesSubject.listen((onData) => print(_cities));
   }
 }
