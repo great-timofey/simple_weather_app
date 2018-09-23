cf138d2e7280ffcb6e934e573df910e9cd11de76
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-09-23 20:02:29 +0700

    fixed city tile for accuweather api

--- lib/components/city_tile.dart
+++ lib/components/city_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'package:simple_weather_app/models/models.dart';
 
@@ -6,6 +7,62 @@ class CityTile extends StatelessWidget {
   final City city;
   final Function onDismiss;
   CityTile(this.city, this.onDismiss);
+
+  Widget _buildDaily(item, index, isLast) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+            right: !isLast ? BorderSide.none : BorderSide(color: Colors.blue)),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+              index == 0
+                  ? 'Today'
+                  : DateFormat('EEEE')
+                      .format(DateTime.now().add(Duration(days: 1 * index))),
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+          Text('Max: ' + item['Temperature']['Maximum'],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.w500,
+              )),
+          Text('Min: ' + item['Temperature']['Minimum'],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.w500,
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHourly(item, index, isLast) => Container(
+        decoration: BoxDecoration(
+          border: Border(
+              right:
+                  !isLast ? BorderSide.none : BorderSide(color: Colors.blue)),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text((DateTime.now().hour + index).toString() +
+                ':' +
+                DateTime.now().minute.toString()),
+            Text(item['Temperature'],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                )),
+          ],
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -18,102 +75,128 @@ class CityTile extends StatelessWidget {
         fit: BoxFit.fitWidth,
       )),
       child: Dismissible(
-        key: Key(city.name),
-        direction: DismissDirection.startToEnd,
-        background: Container(
-            padding: EdgeInsets.only(left: 10.0),
-            color: Colors.red,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Delete',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    )),
-              ],
-            )),
-        onDismissed: (_) {
-          onDismiss(city.name);
-        },
-        child: ExpansionTile(
-          trailing: Container(
-            child: Text(
-              'bal',
-              // city.forecast.currentTemp,
-              style: TextStyle(
-                fontSize: 35.0,
-                fontWeight: FontWeight.w900,
-                // color: Colors.white,
+          key: Key(city.name),
+          direction: DismissDirection.startToEnd,
+          background: Container(
+              padding: EdgeInsets.only(left: 10.0),
+              color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Delete',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      )),
+                ],
+              )),
+          onDismissed: (_) {
+            onDismiss(city.name);
+          },
+          child: ExpansionTile(
+            backgroundColor: Colors.white,
+            trailing: Container(
+              child: Text(
+                // 'bal',
+                city.forecast.current['Temperature']['Metric'] + ' C',
+                style: TextStyle(
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w900,
+                  // color: Colors.white,
+                ),
               ),
             ),
-          ),
-          title: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      city.name,
-                      style: TextStyle(
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.w700,
-                        // color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'sdf',
-                      // city.forecast.currentForecast,
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontStyle: FontStyle.italic,
-                        // color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          children: <Widget>[
-            Container(
-              // padding: const EdgeInsets.all(20.0),
+            title: Container(
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                          padding: EdgeInsets.only(right: 10.0),
-                          child: Text('Hourly:')),
-                      Column(children: [
-                        // Text(city.forecast.hourlyForecast),
-                        // Text(city.forecast.hourlyTemp)
-                      ])
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        city.name,
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.w700,
+                          // color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        // 'sdf',
+                        city.forecast.current['WeatherText'],
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontStyle: FontStyle.italic,
+                          // color: Colors.white,
+                        ),
+                      ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Padding(
-                          padding: EdgeInsets.only(right: 10.0),
-                          child: Text('Daily:')),
-                      Column(children: [
-                        // Text('min ${city.forecast.dailyMinTemp}'),
-                        // Text('max ${city.forecast.dailyMaxTemp}'),
-                      ])
-                    ],
-                  )
                 ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Hourly Forecasts',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.blue),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: (Border(
+                    top: BorderSide(color: Colors.blue),
+                    bottom: BorderSide(color: Colors.blue),
+                  )),
+                ),
+                height: 50.0,
+                child: ListView.builder(
+                  itemCount: city.forecast.hourlyForecasts.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => index ==
+                          city.forecast.hourlyForecasts.length - 1
+                      ? _buildHourly(
+                          city.forecast.hourlyForecasts[index], index, false)
+                      : _buildHourly(
+                          city.forecast.hourlyForecasts[index], index, true),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Daily Forecasts',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.blue),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: (Border(
+                    top: BorderSide(color: Colors.blue),
+                    bottom: BorderSide(color: Colors.blue),
+                  )),
+                ),
+                height: 200.0,
+                child: ListView.builder(
+                  itemCount: city.forecast.daily['DailyForecasts'].length,
+                  itemBuilder: (context, index) =>
+                      index == city.forecast.daily['DailyForecasts'].length - 1
+                          ? _buildDaily(
+                              city.forecast.daily['DailyForecasts'][index],
+                              index,
+                              false)
+                          : _buildDaily(
+                              city.forecast.daily['DailyForecasts'][index],
+                              index,
+                              true),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
